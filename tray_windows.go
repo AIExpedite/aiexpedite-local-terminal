c76dfee5255de7f8145c1697a43848e369a7c5b0
Daniel Kupisz <130611647+DanKupisz@users.noreply.github.com>
2026-01-06 15:57:14 -0500

    chore: Bump version to 0.2.1 in agent and tray_windows files

--- tray_windows.go
+++ tray_windows.go
@@ -184,7 +184,7 @@ func ensureAppRegistration() error {
 	// Set required registry values for Add/Remove Programs
 	values := map[string]string{
 		"DisplayName":     "AI Expedite Terminal",
-		"DisplayVersion":  "0.4.0",
+		"DisplayVersion":  "0.2.1",
 		"Publisher":       "AI Expedite",
 		"InstallLocation": exeDir,
 		"DisplayIcon":     iconPath,
